a27e932078db72dcc68807da54a81d237e555a86
Del0m <98620378+Del0m@users.noreply.github.com>
2024-09-09 03:46:18 +0000

    changed the print statements to match the pdf; cleaned up \n

--- fracturing.c
+++ fracturing.c
@@ -7,31 +7,29 @@
 
 double askForUserInput() { // ask and return double
     double enteredValue;
-
-    printf("\nEnter value: ");
     scanf("%lf", &enteredValue);
 
     return enteredValue;
 }
 
 double calculateDistance() { // calculate the distance between two points with distance formula
-    printf("\nEnter coordinates for the point, x first, y second.");
     double x1 = askForUserInput();
     double y1 = askForUserInput();
     
     // printing out point 1 coordinates
-    printf("\nPoint 1: (%lf,%lf)", x1, y1);
+    printf("Point #1 was entered: x1 = %lf; y1= %lf\n", x1, y1);
 
     double x2 = askForUserInput();
     double y2 = askForUserInput();
 
     // printing out point 2 coordinates
-    printf("\nPoint 2: (%lf,%lf)", x2, y2);
+    printf("Point #2 was entered: x2 = %lf; y2= %lf\n", x2, y2);
+
     
     // using the distance formula
     double distance = pow(pow(x2-x1, 2) + pow(y2-y1, 2), .5);
 
-    printf("\nThe distance between point 1 and point 2 is: %lf", distance);
+    printf("The distance between the two points is %lf\n", distance);
 
     return distance;
 }
@@ -40,7 +38,7 @@ double calculatePerimeter() { // calculate the perimeter of a circle given diame
 
     // 2PI diameter / 2 is circumference of circle
     double circumference = 2*PI*(diameter/2);
-    printf("\nPerimeter of the circle is equal to: %lf", circumference);
+    printf("The perimeter of the city encompassed by your request is %lf\n", circumference);
     return 1;
 }
 double calculateArea() { // calculate area of a circle given diameter
@@ -48,19 +46,19 @@ double calculateArea() { // calculate area of a circle given diameter
 
     // PI (diameter / 2 )^2 is area of a circle formula
     double circleArea = PI * pow((diameter / 2), 2);
-    printf("\nArea of the circle is equal to: %lf", circleArea);
+    printf("The area of the city encompassed by your request is %lf\n", circleArea);
 
     return 1;
 }
 double calculateWidth() { // circle has a same diameter / width everywhere, so calculatedistance is correct in answering this
     double width = calculateDistance();
-    printf("\nThe width of the circle is: %lf", width);
+    printf("The width of the city encompassed by your request is %lf\n", width);
 
     return 3;
 }
 double calculateHeight() { // circle has a same diameter / height everywhere, so calculatedistance is correct in answering this
     double height = calculateDistance();
-    printf("\nThe width of the circle is: %lf", height);
+    printf("The height of the city encompassed by your request is %lf\n", height);
 
     return 3;
 }
